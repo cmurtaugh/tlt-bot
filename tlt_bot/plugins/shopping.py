cbe8bde498449328a97c11db5d24cbcd70e05819
Colin Murtaugh <colin_murtaugh@harvard.edu>
2016-08-18 20:13:46 -0400

    added shopping

---
+++ tlt_bot/plugins/shopping.py
@@ -0,0 +1,17 @@
+from slackbot.bot import listen_to
+import re
+
+
+@listen_to('shopping', re.IGNORECASE)
+def shopping(message):
+    message.reply("Don't you mean Course Selection Period?")
+
+
+@listen_to('shopper', re.IGNORECASE)
+def shopper(message):
+    message.reply("Ahem. Prospective Enrollee?")
+
+
+@listen_to('study card', re.IGNORECASE)
+def studycard(message):
+    message.reply("That's Crimson Cart to you, buddy.")
