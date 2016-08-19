fc433d7c502b2d6a08ce3ab1365a66896d2a1e03
Colin Murtaugh <cmurtaugh@gmail.com>
2016-08-18 20:22:50 -0400

    Update servicenow.py

--- tlt_bot/plugins/servicenow.py
+++ tlt_bot/plugins/servicenow.py
@@ -1,6 +1,10 @@
 from slackbot.bot import listen_to
-
+import re
 
 @listen_to('(INC\d+)')
 def servicenow(message, incident_id):
     message.reply("Here's the URL! https://harvard.service-now.com/nav_to.do?uri=incident.do?sysparm_query=number=%s" % incident_id)
+
+@listen_to('clos[e|ing]\s.*ticket')
+def tickets(message):
+    message.reply(":rose2:")
