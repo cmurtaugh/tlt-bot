18088feaff0bd15830d96ace38f7a866dec0723a
Colin Murtaugh <colin_murtaugh@harvard.edu>
2016-08-23 17:02:09 -0400

    added jira

---
+++ tlt_bot/plugins/jira.py
@@ -0,0 +1,7 @@
+from slackbot.bot import listen_to
+import re
+
+
+@listen_to('(TLT-\d+)', re.IGNORECASE)
+def jira(message, ticket):
+    message.reply("Here's the URL! https://jira.huit.harvard.edu/browse/%s" % incident_id)
