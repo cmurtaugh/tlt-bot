f2814225289bf37ebd6a42ec9b222a5394ce198e
Colin Murtaugh <colin_murtaugh@harvard.edu>
2016-08-23 10:54:59 -0400

    don't blab so much

--- tlt_bot/plugins/shopping.py
+++ tlt_bot/plugins/shopping.py
@@ -1,17 +1,21 @@
 from slackbot.bot import listen_to
 import re
+from random import randint
 
 
 @listen_to('shopping', re.IGNORECASE)
 def shopping(message):
-    message.reply("Don't you mean Course Selection Period?")
+    if randint(1, 10) == 5:
+        message.reply("Don't you mean Course Selection Period?")
 
 
 @listen_to('shopper', re.IGNORECASE)
 def shopper(message):
-    message.reply("Ahem. Prospective Enrollee?")
+    if randint(1, 10) == 5:
+        message.reply("Ahem. Prospective Enrollee?")
 
 
 @listen_to('study card', re.IGNORECASE)
 def studycard(message):
-    message.reply("How about a :crimson_cart:?")
+    if randint(1, 10) == 5:
+        message.reply("How about a :crimson_cart:?")
